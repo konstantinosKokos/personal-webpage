df46b525063cecfc2ff240aa18b6189bf776e820
konstantinosKokos <konstantinos@riseup.net>
2023-06-26 21:39:37 +0300

    tidy up

--- site.hs
+++ site.hs
@@ -1,27 +1,26 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid ((<>))
-import           Control.Monad (liftM)
-import           Hakyll
-import           Text.CSL  as CSL
-import qualified Data.Text as T
---------------------------------------------------------------------------------
+
+import Hakyll
+import qualified Data.Text as Text
+import Text.CSL as CSL
+import Control.Monad (liftM)
+
 main :: IO ()
 main = hakyll $ do
     match "images/*" $ do
-        route   idRoute
+        route idRoute
         compile copyFileCompiler
 
     match "js/*" $ do
-      route idRoute
-      compile copyFileCompiler
+        route idRoute
+        compile copyFileCompiler
 
     match "favicon.ico" $ do
-      route idRoute
-      compile copyFileCompiler
+        route idRoute
+        compile copyFileCompiler
 
     match "css/*" $ do
-        route   idRoute
+        route idRoute
         compile compressCssCompiler
 
     match "cv/cv.pdf" $ do
@@ -37,66 +36,61 @@ main = hakyll $ do
 
     match "pubs/*" $ do
         route idRoute
-        compile $ pandocCompiler
+        compile pandocCompiler
 
     create ["talks.html"] $ do
         route idRoute
         compile $ do
             talks <- recentFirst =<< loadAll "talks/*"
             let archiveCtx =
-                    listField "talks" talkCtx (return talks) `mappend`
-                    constField "title" "Talks" `mappend`
+                    listField "talks" talkCtx (return talks) <>
+                    constField "title" "Talks" <>
                     defaultContext
                     
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/talks.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/main.html" archiveCtx
-                >>= relativizeUrls
+            makeItem "" >>= loadAndApplyTemplate "templates/talks.html" archiveCtx
+                        >>= loadAndApplyTemplate "templates/main.html" archiveCtx
+                        >>= relativizeUrls
                 
     create ["publications.html"] $ do
         route idRoute
         compile $ do
             pubs <- recentFirst =<< loadAll "pubs/*"
             let archiveCtx = 
-                    listField "pubs" pubCtx (return pubs) `mappend` 
-                    constField "title" "Publications" `mappend`
+                    listField "pubs" pubCtx (return pubs) <>
+                    constField "title" "Publications" <>
                     defaultContext
                 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/pubs.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/main.html" archiveCtx
-                >>= relativizeUrls
+            makeItem "" >>= loadAndApplyTemplate "templates/pubs.html" archiveCtx
+                        >>= loadAndApplyTemplate "templates/main.html" archiveCtx
+                        >>= relativizeUrls
 
     match "pages/index.html" $ do
-      route $ (gsubRoute "pages/" (const ""))
-      compile $ do
-        talks <- fmap (take 4) . recentFirst =<< loadAll "talks/*"
-        let indexCtx =
-               listField "talks" talkCtx (return talks) `mappend`
-                defaultContext
-
-        getResourceBody
-               >>= applyAsTemplate indexCtx
-               >>= loadAndApplyTemplate "templates/main.html" indexCtx
-               >>= relativizeUrls
-
-      match "pages/*" $ do
-        route $ (gsubRoute "pages/" (const ""))
+        route $ gsubRoute "pages/" (const "")
         compile $ do
-            getResourceBody
-                >>= loadAndApplyTemplate "templates/main.html" defaultContext
-                >>= relativizeUrls
+            talks <- fmap (take 4) . recentFirst =<< loadAll "talks/*"
+            let indexCtx =
+                    listField "talks" talkCtx (return talks) <>
+                    defaultContext
+
+            getResourceBody >>= applyAsTemplate indexCtx
+                             >>= loadAndApplyTemplate "templates/main.html" indexCtx
+                             >>= relativizeUrls
+
+    match "pages/*" $ do
+        route $ gsubRoute "pages/" (const "")
+        compile $ getResourceBody
+            >>= loadAndApplyTemplate "templates/main.html" defaultContext
+            >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
 
---------------------------------------------------------------------------------
 talkCtx :: Context String
 talkCtx =
-    dateField "date" "%B %e, %Y" `mappend`
+    dateField "date" "%B %e, %Y" <>
     defaultContext
     
 
 pubCtx :: Context String
 pubCtx = 
-    dateField "date" "%B %Y" `mappend`
+    dateField "date" "%B %Y" <>
     defaultContext
