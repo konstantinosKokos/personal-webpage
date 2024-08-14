2c8407a461efb54323b0ed3c2838de72c25094d6
konstantinosKokos <konstantinos@riseup.net>
2024-08-14 14:34:45 +0300

    less awkward js

--- js/collapsible.js
+++ js/collapsible.js
@@ -2,13 +2,9 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click",
-  function() {
+  coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    while(content.className != "abstract") {
-      var content = content.nextElementSibling;
-    }
+    var content = this.parentElement.querySelector(".abstract");
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
